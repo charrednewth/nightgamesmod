d680481c173935981643be9283898a9507010ce3
charrednewth <sollipsis@yahoo.fr>
2015-09-25 23:55:47 +0200

    Player-owned amounts display in shops

--- NightgamesMod/nightgames/daytime/Bookstore.java
+++ NightgamesMod/nightgames/daytime/Bookstore.java
@@ -4,6 +4,7 @@ import nightgames.characters.Character;
 import nightgames.global.Flag;
 import nightgames.global.Global;
 import nightgames.items.Item;
+import java.util.Map;
 
 public class Bookstore extends Store {
 	public Bookstore(Character player) {
